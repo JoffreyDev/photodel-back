3669a93ab4e18bf1ce9fb3eb52de24525ce516b0
Nikita Zaretski <xal9wIII4ik@yandex.ru>
2022-05-13 23:40:33 +0300

    try

--- photodel/photodel/settings.py
+++ photodel/photodel/settings.py
@@ -43,7 +43,7 @@ ROOT_URLCONF = 'photodel.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates'), os.path.join(BASE_DIR, 'build')],
+        'DIRS': [os.path.join(BASE_DIR, 'build'), os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
