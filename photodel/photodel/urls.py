955ae5caa53df37084b8695fb0ccceb46064bfb9
Nikita Zaretski <xal9wIII4ik@yandex.ru>
2022-05-13 23:49:52 +0300

    try

--- photodel/photodel/urls.py
+++ photodel/photodel/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.api.urls', namespace='accounts')),
     path('api/additional_entities/', include('additional_entities.api.urls', namespace='additional_entities')),
