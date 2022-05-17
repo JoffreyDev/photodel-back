40c8d3dcbfe0fdaa88bec9c66d14bce0c8d5e4e5
Nikita Zaretski <xal9wIII4ik@yandex.ru>
2022-05-18 02:12:10 +0300

    add cors

--- photodel/photodel/urls.py
+++ photodel/photodel/urls.py
@@ -5,7 +5,6 @@ from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.api.urls', namespace='accounts')),
     path('api/additional_entities/', include('additional_entities.api.urls', namespace='additional_entities')),
