d88bbe2e05414e4703d117acae41593f1bca4aad
Nikita Zaretski <xal9wIII4ik@yandex.ru>
2022-06-08 16:48:09 +0300

    update

--- photodel/additional_entities/api/serializers.py
+++ photodel/additional_entities/api/serializers.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import AnonymousUser
-from django.db.models import Count
+from django.db.models import Count, F
 
 from rest_framework import serializers
 
@@ -56,19 +56,18 @@ class QuestionListSerializer(serializers.ModelSerializer):
         fields = ['title', 'is_hide', 'choices', 'count_answer', 'is_vote', ]
 
     def get_choices(self, obj):
-        choice_serializer = ChoiceListSerializer(many=True)
-        return choice_serializer.to_representation(Choice.objects.filter(question=obj.id))
+        return Answer.objects.filter(choice__question=obj.id)\
+            .values('choice__title', 'choice__id')\
+            .annotate(total=Count('choice__title'))\
+            .order_by('total')
 
     def get_count_answer(self, obj):
-        return Answer.objects.filter(choice__question=obj.id) \
-            .values('choice__title') \
-            .annotate(total=Count('choice__title')) \
-            .order_by('total')
+        return Answer.objects.filter(choice__question=obj.id).count()
 
     def get_is_vote(self, obj):
         if isinstance(self.context.get('user'), AnonymousUser):
             return ''
-        return bool(Answer.objects.filter(choice__question=obj.id, profile__user=self.context.get('user')))
+        return Answer.objects.filter(choice__question=obj.id, profile__user=self.context.get('user')).exists()
 
 
 class AnswerCreateSerializer(serializers.ModelSerializer):
