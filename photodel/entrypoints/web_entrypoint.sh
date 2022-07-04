b1180c0957200c424a3c826fd9cdee528d16403f
Nikita Zaretski <xal9wIII4ik@yandex.ru>
2022-07-04 15:56:25 +0300

    try to change database

--- photodel/entrypoints/web_entrypoint.sh
+++ photodel/entrypoints/web_entrypoint.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
-if [ "$DATABASE" = "postgres" ]
-  then
-    echo "Waiting for psql"
+#if [ "$DATABASE" = "postgres" ]
+#  then
+#    echo "Waiting for psql"
+#
+#    while ! nc -z $SQL_HOST $SQL_PORT; do
+#      sleep 0.1
+#    done
+#
+#    echo "Psql started"
+#fi
 
-    while ! nc -z $SQL_HOST $SQL_PORT; do
-      sleep 0.1
-    done
-
-    echo "Psql started"
-fi
-
+python manage.py migrate --fake
 python manage.py migrate
 python manage.py collectstatic --noinput
 python manage.py runserver 0.0.0.0:8000
