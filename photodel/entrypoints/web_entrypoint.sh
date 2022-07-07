711e6f89c968bcdc91603393b1941dead5c7a471
Nikita Zaretski <xal9wIII4ik@yandex.ru>
2022-07-08 01:43:24 +0300

    .

--- photodel/entrypoints/web_entrypoint.sh
+++ photodel/entrypoints/web_entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-python manage.py migrate --fake
+#python manage.py migrate --fake
 python manage.py migrate
 python manage.py collectstatic --noinput
 python manage.py runserver 0.0.0.0:8000
