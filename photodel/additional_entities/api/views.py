33eaf684b46b84735f3e008449f5d40c5fcf7dc5
Plotko Eduard <eduard.plotko@fors.ru>
2024-05-11 04:26:43 +0300

    fixes

--- photodel/additional_entities/api/views.py
+++ photodel/additional_entities/api/views.py
@@ -3,6 +3,7 @@ from rest_framework import viewsets, status, permissions
 from additional_entities.models import Country, Language, Advertisement, City, Question
 from film_places.models import FilmPlacesComment
 from gallery.models import GalleryComment, PhotoSessionComment
+from trainings.models import TrainingsComment
 from accounts.models import Profile
 from additional_entities.models import CustomSettings
 from .serializers import CountryListSerializer, LanguageListSerializer, \
@@ -12,6 +13,9 @@ from rest_framework.response import Response
 from services.additional_service import check_town_use_coords, check_exitst_answer
 import datetime
 import math
+from django.http import JsonResponse
+from itertools import chain
+from operator import attrgetter
 
 
 class CountryViewSet(viewsets.ViewSet):
@@ -99,21 +103,30 @@ class PollViewSet(viewsets.ViewSet):
 
 class CommonViewSet(viewsets.ViewSet):
 
-    def list_last_comments(self, request):
-        photo_comment = {'photo_comment':
-                         GalleryComment.objects.values('content', 'timestamp', 'sender_comment__name',
-                                                       'sender_comment__surname', 'sender_comment__status',
-                                                       'gallery__name_image', 'sender_comment__id', 'gallery__id', 'sender_comment__user_channel_name').last()
-                         }
+ def list_last_comments(self, request):
+    # Получаем все комментарии
+    photo_comments = GalleryComment.objects.all().values('content', 'timestamp', 'sender_comment__name',
+    'sender_comment__surname', 'sender_comment__status',
+    'gallery__name_image', 'sender_comment__id', 'gallery__id',
+    'sender_comment__user_channel_name')
+    photo_session_comments = PhotoSessionComment.objects.all().values('content', 'timestamp', 'sender_comment__name',
+    'sender_comment__surname', 'sender_comment__status',
+    'photo_session__session_name', 'sender_comment__id',
+    'photo_session__id', 'sender_comment__user_channel_name')
+    place_comments = FilmPlacesComment.objects.all().values('content', 'timestamp', 'sender_comment__name',
+    'sender_comment__surname', 'sender_comment__status',
+    'place__name_place', 'sender_comment__id', 'place__id',
+    'sender_comment__user_channel_name')
+    training_comments = TrainingsComment.objects.all().values('content', 'timestamp', 'sender_comment__name',
+    'sender_comment__surname', 'sender_comment__status',
+    'training__training_title', 'sender_comment__id', 'training__id',
+    'sender_comment__user_channel_name')
 
-        photo_comment.update({'photo_session_comment':
-                              PhotoSessionComment.objects.values('content', 'timestamp', 'sender_comment__name',
-                                                                 'sender_comment__surname', 'sender_comment__status',
-                                                                 'photo_session__session_name', 'sender_comment__id', 'photo_session__id', 'sender_comment__user_channel_name').last()
-                              })
-        photo_comment.update({'place_comment':
-                              FilmPlacesComment.objects.values('content', 'timestamp', 'sender_comment__name',
-                                                               'sender_comment__surname', 'sender_comment__status',
-                                                               'place__name_place', 'sender_comment__id', 'place__id', 'sender_comment__user_channel_name').last()
-                              })
-        return Response(status=status.HTTP_200_OK, data=photo_comment)
+    # Объединяем все комментарии в один список
+    all_comments = list(chain(photo_comments, photo_session_comments, place_comments, training_comments))
+
+    # Сортируем комментарии по дате создания и берем последние три
+    last_comments = sorted(all_comments, key=lambda x: x['timestamp'], reverse=True)[:3]
+
+    # Возвращаем данные в формате JSON
+    return JsonResponse(last_comments, safe=False)
